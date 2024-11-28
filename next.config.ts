03a3845f763f53f30fe1c8f3e5491db29e79a47c
hari-hk <hariharakumar5196@gmail.com>
2024-11-28 21:53:12 +0530

    fix: update build configuration

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export', 
+  output: 'export',
+  distDir: 'build',
 };
 
 export default nextConfig;
